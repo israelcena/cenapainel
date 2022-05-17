c39f0cb55fac595023c29c1a7a405bdc2c0cf577
Israel Cena <israelsjm@gmail.com>
2022-05-17 09:08:00 -0300

    remove default components

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World!/);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("adds 1 + 2 to equal 3", () => {
+	expect(1+2).toBe(3);
 });
