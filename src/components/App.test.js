c4f1a3984c07aed001f9fd065894aa688241267f
Israel Cena <israelsjm@gmail.com>
2022-05-19 10:02:37 -0300

    comment hello World

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World!/);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Hello World!/);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 test("adds 1 + 2 to equal 3", () => {
 	expect(1+2).toBe(3);
