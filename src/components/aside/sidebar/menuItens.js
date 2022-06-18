3fb32c521873b3ab9f3c879d0d96e59fc368f421
Israel Cena <israelsjm@gmail.com>
2022-06-18 20:59:27 -0300

    include message menu item

--- src/components/aside/sidebar/menuItens.js
+++ src/components/aside/sidebar/menuItens.js
@@ -7,14 +7,21 @@ export const menuItens = [
 	},
 	{
 		id: "2",
-		name: "About",
+		name: "Mensagens",
 		icon: "MaterialDesign.MdOutlineDashboard",
 		path: "/about",
+		alert: 12,
 	},
 	{
 		id: "3",
-		name: "Contact",
+		name: "Sobre nós",
 		icon: "MaterialDesign.MdOutlineDashboard",
-		path: "/contact",
+		path: "/sobre",
+	},
+	{
+		id: "4",
+		name: "Contato",
+		icon: "MaterialDesign.MdOutlineDashboard",
+		path: "/contato",
 	},
 ];
