be667fa23ee98d830648b4cadddcc3a6610388a2
Israel Cena <israelsjm@gmail.com>
2022-06-19 17:22:19 -0300

    include iconColor via props

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+// components
 import Insight from "./insights/Insight";
 
 // styles
@@ -16,18 +17,21 @@ export default function Dashboard() {
 					amount={"1020,00"}
 					percentage={81}
 					time={"Ultimas 24 horas"}
+					iconColor={"#7380cc"}
 				/>
 				<Insight
 					title={"Gastos"}
 					amount={"1110,00"}
 					percentage={81}
 					time={"Ultimas 24 horas"}
+					iconColor={"#ff7782"}
 				/>
 				<Insight
 					title={"Lucros"}
 					amount={"16540,00"}
 					percentage={21}
 					time={"Ultimas 24 horas"}
+					iconColor={"#42f1b6"}
 				/>
 			</div>
 		</main>
