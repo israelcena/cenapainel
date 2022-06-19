92e674ff357d1a0d2dd17fc8c8015e7e12fad546
Israel Cena <israelsjm@gmail.com>
2022-06-19 13:26:40 -0300

    fix div size

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,26 +6,26 @@ export default function Dashboard() {
 			<h1>Dashboard</h1>
 			<div className="date">
 				<input type="date" />
-				<div className="insights">
-					<Insight
-						title={"Vendas"}
-						amount={"1020,00"}
-						percentage={81}
-						time={"Ultimas 24 horas"}
-					/>
-					<Insight
-						title={"Gastos"}
-						amount={"1110,00"}
-						percentage={81}
-						time={"Ultimas 24 horas"}
-					/>
-					<Insight
-						title={"Lucros"}
-						amount={"16540,00"}
-						percentage={21}
-						time={"Ultimas 24 horas"}
-					/>
-				</div>
+			</div>
+			<div className="insights">
+				<Insight
+					title={"Vendas"}
+					amount={"1020,00"}
+					percentage={81}
+					time={"Ultimas 24 horas"}
+				/>
+				<Insight
+					title={"Gastos"}
+					amount={"1110,00"}
+					percentage={81}
+					time={"Ultimas 24 horas"}
+				/>
+				<Insight
+					title={"Lucros"}
+					amount={"16540,00"}
+					percentage={21}
+					time={"Ultimas 24 horas"}
+				/>
 			</div>
 		</main>
 	);
