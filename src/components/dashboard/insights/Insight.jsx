eb18a69ec69fe9f82005333236abc3fbb252185b
Israel Cena <israelsjm@gmail.com>
2022-06-19 18:15:37 -0300

    ninclude insight component

--- src/components/dashboard/insights/Insight.jsx
+++ src/components/dashboard/insights/Insight.jsx
@@ -1,16 +1,37 @@
+// icons
 import { MdAnalytics } from "react-icons/md";
-export default function Insight({ title, amount, percentage, time }) {
+
+// styles
+import "./insightStyles.css";
+export default function Insight({
+	title,
+	amount,
+	percentage,
+	time,
+	icon,
+	iconColor,
+}) {
 	return (
 		<div className="insight">
-			<MdAnalytics />
+			<MdAnalytics
+				className="insight--icon"
+				style={{ background: `${iconColor}` }}
+			/>
 			<div className="insights--main-content">
 				<div className="insights--text">
 					<h3>{title}</h3>
-					<p className="insights--text-amount">R$ {amount}</p>
+					<p>R$ {amount}</p>
 				</div>
 				<div className="insights--progress">
 					<svg>
-						<circle cx="38" cy="38" r="36" />
+						<circle
+							cx="38"
+							cy="38"
+							r="36"
+							stroke-dasharray="220"
+							stroke-dashoffset="20"
+							stroke-width="14"
+						/>
 					</svg>
 					<div className="insights--percentage">
 						<p>{percentage}%</p>
