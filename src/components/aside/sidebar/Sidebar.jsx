e91a2ad65f3708e08e480986024a858f38fed92f
Israel Cena <israelsjm@gmail.com>
2022-06-27 09:17:11 -0300

    change icons

--- src/components/aside/sidebar/Sidebar.jsx
+++ src/components/aside/sidebar/Sidebar.jsx
@@ -10,14 +10,48 @@ import "./sidebarStyles.css";
 export default function Sidebar() {
 	return (
 		<div className="sidebar">
-			{menuItens.map((item) => {
-				return (
-					<a key={item.id} className="nav-itens" href={item.path}>
-						<MaterialDesign.MdOutlineDashboard className="menu-icon" />
-						<h3>{item.name}</h3>
-						{item.alert ? <span className="message-count">{item.alert}</span> : null}
-					</a>
-				);
+			{menuItens.map(({ id, path, name, alert }) => {
+				switch (name) {
+					case "Dashboard":
+						return (
+							<a key={id} className="nav-itens" href={path}>
+								<MaterialDesign.MdOutlineDashboard className="menu-icon" />
+								<h3>{name}</h3>
+								{alert ? <span className="message-count">{alert}</span> : null}
+							</a>
+						);
+					case "Mensagens":
+						return (
+							<a key={id} className="nav-itens" href={path}>
+								<MaterialDesign.MdMessage className="menu-icon" />
+								<h3>{name}</h3>
+								{alert ? <span className="message-count">{alert}</span> : null}
+							</a>
+						);
+					case "Sobre nós":
+						return (
+							<a key={id} className="nav-itens" href={path}>
+								<MaterialDesign.MdOutlineAssignmentInd className="menu-icon" />
+								<h3>{name}</h3>
+								{alert ? <span className="message-count">{alert}</span> : null}
+							</a>
+						);
+					case "Contato":
+						return (
+							<a key={id} className="nav-itens" href={path}>
+								<MaterialDesign.MdContactSupport className="menu-icon" />
+								<h3>{name}</h3>
+								{alert ? <span className="message-count">{alert}</span> : null}
+							</a>
+						);
+					default:
+						return (
+							<a key={id} className="nav-itens" href={path}>
+								<h3>{name}</h3>
+								{alert ? <span className="message-count">{alert}</span> : null}
+							</a>
+						);
+				}
 			})}
 			<a class="nav-itens active" href="/logout">
 				<MaterialDesign.MdLogout className="menu-icon" />
